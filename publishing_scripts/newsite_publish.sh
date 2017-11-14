d8c7629a898104d006466d063367da91d176cd8c
Simone Sturniolo <4365814+stur86@users.noreply.github.com>
2017-11-14 12:17:05 +0000

    Improved newsite publish script

--- publishing_scripts/newsite_publish.sh
+++ publishing_scripts/newsite_publish.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Folders or files to NOT archive
+EXCLUDED_PATHS='publishing_scripts' 
+
 # Requires to pass the folder where the ccpnc-site git is located as an
 # argument
 
 rm -rf $1/grav-admin/magresview/magresview
-hg archive $1/grav-admin/magresview/magresview
+hg archive -X "$EXCLUDED_PATHS" $1/grav-admin/magresview/magresview
